0cc459524863d929a9a898fcbeaaaff7a53a7f49
Milena valente <Milena_frey@estudante.sesisenai.org.org>
2026-06-18 22:03:47 -0300

    atividade

--- AtividadesCondicionais.cs
+++ AtividadesCondicionais.cs
@@ -2,6 +2,46 @@ public static class AtividadesCondicionais
 {
     public static void Atividade01()
     {
-        Console.WriteLine("Atividade 1");
-    }
+        Console.Write("Informe a idade do visitante: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 16)
+{
+    Console.WriteLine("Entrada liberada.");
 }
+else
+{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}
+    }
+
+    public static void Atividade02()
+    {
+Console.Write ("Informe a idade do visitante:");
+int idade = int.Parse(Console.ReadLine());
+Console.Write("Informe a nota final: ");
+double nota = double.Parse(Console.ReadLine());
+
+if (idade >= 16)
+if (nota >= 7)
+{
+    Console.WriteLine("Entrada liberada.");
+    Console.WriteLine("Estudante aprovado.");
+}
+else if (nota >= 5)
+{
+    Console.WriteLine("Estudante em recuperação.");
+}
+else
+{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+    Console.WriteLine("Estudante reprovado.");
+}
+    }
+
+    public static void Atividade03()
+    {
+ Console.Write("Classificação de temperatura ambiente:")
+    }
+  
+
